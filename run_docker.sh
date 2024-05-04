d09964542af611498b8dd124aa9a17349941ca52
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2024-05-04 23:44:34 +0200

    Update run_docker.sh #2

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,28 @@
-#!/bin/zsh
+#!/bin/bash
 
-docker run -dp 0.0.0.0:3000:3000 -v .:/elektropolnilnice-backend --restart=unless-stopped elp1
+# Name of the Docker image
+IMAGE_NAME="elp-backend"
+# Name of the Docker container
+CONTAINER_NAME="back"
+
+# Check if a container with the specified image name is running
+if docker ps -q -f "ancestor=$IMAGE_NAME" >/dev/null; then
+    # If running, stop and remove the container
+    echo "Container with image $IMAGE_NAME is running. Stopping and removing it."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+    docker rmi $IMAGE_NAME
+else
+    # If not running, check if the image exists
+    if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
+        # If the image exists, start a container with it
+        echo "Image $IMAGE_NAME exists. Starting container."
+        docker run -d - $CONTAINER_NAME $IMAGE_NAME
+    else
+        # If the image doesn't exist, build it from Dockerfile and start a container
+        echo "Image $IMAGE_NAME does not exist. Building it and starting container."
+        docker build -t $IMAGE_NAME .
+        docker run -d --name $CONTAINER_NAME $IMAGE_NAME
+    fi
+fi
+
