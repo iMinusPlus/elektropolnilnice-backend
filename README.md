c76dd60ca7885c52ae78a81bef9b9c59c99fe382
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2024-05-05 11:05:13 +0200

    Dodani dodatni ukazi za zagon

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 - osnovno testno vracanje z "http://localhost:3000/polnilnice/test"
 - na serverju test: http://52.174.127.46:3000/polnilnice/test
 - lokalno zaganjanje dockerja v terminalu: docker build -t backend . && docker run -p 3000:3000 backend
+- zaganjanje dockerja na serverju: docker ./run_docker upgrade (ali ./run_docker run|drun|dupgrade -> drun in dupgrade sta za debuggiranje, run in upgrade pa za zagon brez konzole)
+- 
 
 ### TODO:
 - vzpostaviti API za vracanje podatkov
--- run_docker.sh
+++ run_docker.sh
@@ -10,11 +10,11 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-if [ "$1" = "run" ]; then
+if [ "$1" = "drun" ]; then
     echo "Running the application... (Use update if there is new code)"
     # Only run if app was stopped
     docker run -p 0.0.0.0:3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
-elif [ "$1" = "update" ]; then
+elif [ "$1" = "dupgrade" ]; then
     echo "Restarting the application..."
     # Stop and remove all old images and containers
     docker stop $CONTAINER_NAME
@@ -23,8 +23,21 @@ elif [ "$1" = "update" ]; then
     # Build a new ones and start them up
     docker build -t $IMAGE_NAME .
     docker run -p 0.0.0.0:3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
+elif [ "$1" = "upgrade" ]; then
+    echo "Restarting detatched the application..."
+    # Stop and remove all old images and containers
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+    docker rmi $IMAGE_NAME
+    # Build a new ones and start them up
+    docker build -t $IMAGE_NAME .
+    docker run -dp 0.0.0.0:3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
+elif [ "$1" = "run" ]; then
+    echo "Running detached application... (Use dupgrade if there is new code)"
+        # Only run if app was stopped
+        docker run -dp 0.0.0.0:3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
 else
     echo "Invalid argument: $1"
-    echo "Usage: $0 [run|update]"
+    echo "Usage: $0 [run|update|dupgrade|drun]"
     exit 1
 fi
